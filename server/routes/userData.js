57980b3a4e348dac3b971893b87937bd8877d437
appano1 <thankfather@naver.com>
2020-04-02 18:59:16 +0900

    오타를 수정하고 에러 출력을 추가했습니다.

--- server/routes/userData.js
+++ server/routes/userData.js
@@ -5,9 +5,9 @@ const { predictPreference } = require("../predictPreference");
 const router = express.Router();
 
 router.post("/nutrition", (req, res) => {
-  db.query(`read_user_nutirtion'${req.body.userNumber}'`,
+  db.query(`read_user_nutrition'${req.body.userNumber}'`,
     (err, rows) => {
-    if (err) console.log("error");
+    if (err) console.log("Nutrition 정보를 가져오는데 실패했습니다.");
     else {
       res.send(rows.recordsets[0][0]);
     }
@@ -15,10 +15,9 @@ router.post("/nutrition", (req, res) => {
 });
 
 router.post("/intake", (req, res) => {
-  // 1을 유저 번호로 수젛애햐합니다.
   db.query(`read_user_today_nutrition'${req.body.userNumber}'`,
     (err, rows) => {
-    if (err) console.log("error");
+    if (err) console.log("Intake 정보를 가져오는데 실패했습니다.");
     else {
       res.send(rows.recordsets[0][0]);
     }
