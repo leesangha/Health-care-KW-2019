b68eea8a17872f90fb35d41cef97c38ee3887120
appano1 <thankfather@naver.com>
2020-04-03 19:04:25 +0900

    json 형식의 파일 공유로 수정

--- server/recommendation-model/recommend_test01.py
+++ server/recommendation-model/recommend_test01.py
@@ -1,15 +1,32 @@
 import recommendation_model_np as RM
 import numpy as np
+import json
 import sys
 
-R = sys.argv[1].split(',')
-shape = sys.argv[2].split(',')
-shape = list(map(int, shape))
-R = np.array(R, dtype=np.float32).reshape([shape[0], shape[1]])
+# R = sys.argv[1].split(',')
+# shape = sys.argv[2].split(',')
+# shape = list(map(int, shape))
+# R = np.array(R, dtype=np.float32).reshape([shape[0], shape[1]])
+
+# model = RM.RecommendModel(R)
+# model.train()
+
+# result = model.predict(int(sys.argv[3]))
+# for num in result:
+#     print(num)
+
+
+jsonString = sys.stdin.readline()
+dict = json.loads(jsonString)
+
+R = dict['preference']
+userNumber = dict['userNumber']
+
+R = np.array(R, dtype=np.float32)
 
 model = RM.RecommendModel(R)
 model.train()
+predicted = list(model.predict(userNumber))
 
-result = model.predict(int(sys.argv[3]))
-for num in result:
-    print(num)
+result = { "predicted": predicted }
+print(json.dumps(result))
