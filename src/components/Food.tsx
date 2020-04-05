bced182bb4973d5e3fc8f7468beaf97b1c424555
appano1 <thankfather@naver.com>
2020-04-05 22:01:36 +0900

    음식 위에 마우스가 올라갔을 때 음식 번호가 출력

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -2,30 +2,41 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTired, faGrinHearts } from "@fortawesome/free-solid-svg-icons";
 import "./scss/Food.scss";
+import getUserNumber from "./getUserNumber";
 
 type FoodProps = {
   imageSrc: string;
-  num?: number;
+  foodNumber: number;
 };
 
-function Food({ imageSrc, num }: FoodProps) {
-  const food_no = imageSrc.split(".")[0].split("/")[2];
+function Food({ imageSrc, foodNumber }: FoodProps) {
+  // const food_no = imageSrc.split(".")[0].split("/")[2];
+  const [mouseOver, setMouseOver] = useState<boolean>(false);
+  const info = {
+    userNumber: getUserNumber(),
+    foodNumber,
+  };
 
-  const [inputs, setInputs] = useState({
-    user_id: num,
-    food_id: food_no,
-  });
-
-  const onClick = () => {
-    console.log("click event " + num + " " + food_no);
-    setInputs({
-      user_id: num,
-      food_id: food_no,
-    });
-    //DB 선호도 내리기
-    fetch("/hate", {
+  const like = () => {
+    fetch("/food/like", {
       method: "POST",
-      body: JSON.stringify(inputs),
+      body: JSON.stringify(info),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("선호도 올림.");
+      });
+  };
+
+  const dislike = () => {
+    //DB 선호도 내리기
+    fetch("/food/dislike", {
+      method: "POST",
+      body: JSON.stringify(info),
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
@@ -41,17 +52,27 @@ function Food({ imageSrc, num }: FoodProps) {
     <div className="food-container">
       <div className="food">
         <div className="box">
-          <img src={imageSrc} alt="foodImage" />
+          {mouseOver ? <p>{foodNumber}</p> : null}
+          <img
+            src={imageSrc}
+            alt="foodImage"
+            onMouseOver={() => setMouseOver(true)}
+            onMouseOut={() => setMouseOver(false)}
+          />
           <div>
             <button
               className="like-button"
-              onClick={() => console.log('button clicked!')}
+              onClick={like}
             >
-              <FontAwesomeIcon size="2x" color="rgb(255, 202, 0)" icon={faGrinHearts} />
+              <FontAwesomeIcon
+                size="2x"
+                color="rgb(255, 202, 0)"
+                icon={faGrinHearts}
+              />
             </button>
             <button
               className="dislike-button"
-              onClick={onClick}
+              onClick={dislike}
             >
               <FontAwesomeIcon size="2x" color="rgb(255, 202, 0)" icon={faTired} />
             </button>
