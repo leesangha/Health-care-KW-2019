48d52a17ee67d0e16345b19560933f23ff2c4b0c
appano1 <thankfather@naver.com>
2020-04-06 18:32:15 +0900

    typescript 도입

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import Header from "../components/Header";
 import "./Register.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import getUserNumber from "../components/getUserNumber";
 
-function Register(props) {
-  const register = (regInfo) => {
-    fetch("/uploads", {
+type FileStateType = {
+  file: File | null;
+  previewURL: string | null;
+};
+
+function Register() {
+  const userNumber = getUserNumber();
+
+  const register = (regInfo: FormData) => {
+    fetch("/file/uploads", {
       method: "post",
       body: regInfo,
     })
@@ -14,47 +22,66 @@ function Register(props) {
       .then((data) => alert(data.msg));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement> & {
+      target: { img: { files: FileList } };
+    }
+  ) => {
     e.preventDefault();
     const formData = new FormData();
 
-    const userNumber = JSON.parse(sessionStorage.getItem("info"))[0].user_no;
-
-    formData.append("id", userNumber);
+    formData.append("id", userNumber.toString());
     formData.append("img", e.target.img.files[0]);
 
     register(formData);
   };
 
-  const [state, setState] = useState({
-    file: "",
-    previewURL: "",
+  const [state, setState] = useState<FileStateType>({
+    file: null,
+    previewURL: null,
   });
-  const [preview, setPreview] = useState(null);
+  const [preview, setPreview] = useState<JSX.Element | null>(null);
 
   useEffect(() => {
-    if (state.file !== "")
+    if (state.file !== null)
       setPreview(
-        <img className="preview" src={state.previewURL} alt="미리보기 이미지" />
+        <img
+          className="preview"
+          src={state.previewURL!}
+          alt="미리보기 이미지"
+        />
       );
   }, [state]);
 
-  const onChange = (e) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
+    if (e.target.files === null) return;
+
     let reader = new FileReader();
     let file = e.target.files[0];
     reader.onloadend = () =>
       setState({
         file: file,
-        previewURL: reader.result,
+        previewURL: reader.result as string,
       });
     reader.readAsDataURL(file);
   };
+
+  useEffect(() => {
+    fetch("/file/history", {
+      method: "POST",
+      body: JSON.stringify({ userNumber }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }).then((res: Response) => console.log(res.body));
+  }, [userNumber]);
 
   return (
     <>
       <Header />
       <section className="register-page">
+        <article></article>
         <form onSubmit={handleSubmit} encType="multipart/form-data">
           <h1>파일 업로드</h1>
           <input
