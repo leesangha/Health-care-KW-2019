487584fad23546b0b3a2adb7e609ba279c9d67ff
Woochanhyeok <dncksgur1@naver.com>
2020-04-09 13:57:11 +0900

    Statistics에서 db 받아오는 코드 작성

--- src/routes/Statistics.js
+++ src/routes/Statistics.js
@@ -1,43 +1,62 @@
 import React, { useEffect, useState } from "react";
-import {Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+import getUserNumber from "../components/getUserNumber";
 
 function Statistics(props) {
-    const user_name = JSON.parse(sessionStorage.getItem("info"))[0].user_name;
-    const state = {
-        labels: ['January', 'February', 'March',
-                 'April', 'May'],
-        datasets: [
-          {
-            label: 'Rainfall',
-            backgroundColor: 'rgba(75,192,192,1)',
-            borderColor: 'rgba(0,0,0,1)',
-            borderWidth: 2,
-            data: [65, 59, 80, 81, 56]
-          }
-        ]
-      }
+  const user_name = JSON.parse(sessionStorage.getItem("info"))[0].user_name;
+  const user_no = JSON.parse(sessionStorage.getItem("info"))[0].user_no;
   
-      return (
-        <div>
-          <Bar
-            data={state}
-            width={300}
-            height={50}
-            options={{
-              title:{
-                display:true,
-                text:user_name + '의 일주일 통계',
-                fontSize:20
-              },
-              legend:{
-                display:true,
-                position:'top'
-              }
-            }}
-          />
-        </div>
-      );
+  useEffect(() => {
+    console.log(user_no);
+    fetch("/userData/intake", {
+      method: "POST",
+      body: JSON.stringify({ user_no }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+  }, [user_no]);
+
+  const state = {
+    labels: ['January', 'February', 'March',
+      'April', 'May'],
+    datasets: [
+      {
+        label: 'Rainfall',
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56]
+      }
+    ]
   }
+
+  return (
+    <div>
+      <Bar
+        data={state}
+        width={300}
+        height={50}
+        options={{
+          title: {
+            display: true,
+            text: user_name + '의 일주일 통계',
+            fontSize: 20
+          },
+          legend: {
+            display: true,
+            position: 'top'
+          }
+        }}
+      />
+    </div>
+  );
+}
 
 
 export default Statistics;
