3c10229f393633da324f1cb07c9b1d168a3c1289
appano1 <thankfather@naver.com>
2020-04-09 15:30:13 +0900

    Sort 부분 클라이언트 코드로 분리

--- server/predictPreference.js
+++ server/predictPreference.js
@@ -1,35 +1,5 @@
 const { recommend } = require("./recommendation-model/recommend");
 
-function quickSort(list, left = 0, right = list.length - 1) {
-  if (left >= right) return;
-  const mid = Math.floor((left + right) / 2);
-  const pivot = list[mid].predicted_preference;
-  const divide = (list, left, right, pivot) => {
-    while (left <= right) {
-      while (list[left].predicted_preference > pivot) {
-        left++;
-      }
-      while (list[right].predicted_preference < pivot) {
-        right--;
-      }
-      if (left <= right) {
-        let swap = list[left];
-        list[left] = list[right];
-        list[right] = swap;
-        left++;
-        right--;
-      }
-    }
-    return left;
-  };
-  const partition = divide(list, left, right, pivot);
-
-  quickSort(list, left, partition - 1);
-  quickSort(list, partition, right);
-
-  return list;
-}
-
 async function predict(preference, foodNumberList, userNumber) {
   try {
     let predicted_preference = await recommend(preference, userNumber);
@@ -39,7 +9,7 @@ async function predict(preference, foodNumberList, userNumber) {
       predicted_preference: item
     }));
 
-    return quickSort(predicted_preference);
+    return predicted_preference;
   } catch (err) {
     console.error(err);
     // 에러 처리를 나중에 해야됩니다.
