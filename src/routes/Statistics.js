3b0e5895284ee82dff63fb8fb78d9bafccf12a32
appano1 <thankfather@naver.com>
2020-04-11 21:11:37 +0900

    user_no가 넘어가지 않던 오류 수정

--- src/routes/Statistics.js
+++ src/routes/Statistics.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Bar } from 'react-chartjs-2';
 import getUserNumber from "../components/getUserNumber";
 
 function Statistics(props) {
   const user_name = JSON.parse(sessionStorage.getItem("info"))[0].user_name;
-  const user_no = JSON.parse(sessionStorage.getItem("info"))[0].user_no;
-  
+
   useEffect(() => {
-    console.log(user_no);
-    fetch("/userData/intake", {
-      method: "POST",
-      body: JSON.stringify({ user_no }),
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+    const user_no = getUserNumber();
+
+    if(user_no !== -1) {
+      console.log(user_no);
+      fetch("/userData/intake", {
+        method: "POST",
+        body: JSON.stringify({ userNumber: user_no }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        }
       })
-  }, [user_no]);
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        })
+    }
+  }, []);
 
   const state = {
     labels: ['January', 'February', 'March',
@@ -34,7 +37,7 @@ function Statistics(props) {
         data: [65, 59, 80, 81, 56]
       }
     ]
-  }
+  };
 
   return (
     <div>
