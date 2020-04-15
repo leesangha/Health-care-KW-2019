369253f267e430dcbf391e78ca6199daf377fc1a
appano1 <thankfather@naver.com>
2020-04-15 22:30:27 +0900

    분석 결과를 화면에 출력

--- src/components/FoodAnalysis.tsx
+++ src/components/FoodAnalysis.tsx
@@ -1,24 +1,41 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./scss/FoodAnalysis.scss";
 import { CircleLoader } from "react-spinners";
 
 type PropsType = {
-  preview: JSX.Element | null;
+  preview: JSX.Element | null,
+  result: {
+    label: string
+  }[] | null
 };
 
 enum State {
   WAITING,
   LOADING,
   SUCCESS,
-  ERROR,
+  NOT_FOOD
 }
 
-function FoodAnalysis({ preview }: PropsType) {
+function FoodAnalysis({ preview, result }: PropsType) {
   const [state, setState] = useState<State>(State.WAITING);
 
   const submitForm = () => {
     setState(State.LOADING);
   };
+
+  useEffect(() => {
+    if (result === null) {
+      setState(State.WAITING);
+    } else if (result.length === 0) {
+      setState(State.NOT_FOOD);
+    } else {
+      setState(State.SUCCESS);
+    }
+  }, [result]);
+
+  useEffect(() => {
+    setState(State.WAITING);
+  }, [preview]);
 
   return preview === null ? null : (
     <div className="submit-button-wrapper">
@@ -34,7 +51,12 @@ function FoodAnalysis({ preview }: PropsType) {
             <CircleLoader />
           </div>
         )
-        : null}
+        : state === State.SUCCESS
+          ? result?.map(({label}, index) => <p key={index}>{label}</p>)
+          : state === State.NOT_FOOD
+            ? <p>음식이 아닙니다.</p>
+            : null
+      }
     </div>
   );
 }
