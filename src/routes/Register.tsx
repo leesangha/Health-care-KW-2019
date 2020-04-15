9b4f55d5e345065abcfe4ce92250017cc38a5829
appano1 <thankfather@naver.com>
2020-04-15 20:58:30 +0900

    자식 컴포넌트로 전달할 변수 추가

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import Header from "../components/Header";
 import "./Register.scss";
 import getUserNumber from "../components/getUserNumber";
@@ -7,12 +7,16 @@ import UserFoodHistory from "../components/UserFoodHistory";
 
 function Register() {
   const userNumber = useMemo(() => getUserNumber(), []);
+  const [isUpload, setUploadState] = useState(false);
 
   return (
     <div className="register-page-container">
       <Header />
-      <FileUpload userNumber={userNumber} />
-      <UserFoodHistory userNumber={userNumber} />
+      <FileUpload userNumber={userNumber} setUploadState={setUploadState} />
+      <UserFoodHistory
+        userNumber={userNumber}
+        uploadState={[isUpload, setUploadState]}
+      />
     </div>
   );
 }
