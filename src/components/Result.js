b5776e53631a587632447aeadfd3b2f28d0d65a5
appano1 <thankfather@naver.com>
2020-04-21 12:41:40 +0900

    prettier 적용

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import Button from '@material-ui/core/Button'
+import React from "react";
+import Button from "@material-ui/core/Button";
 
-function Result({list, onRemove}){
-    return (
-        <div>
-            {list.map((item, index) =>(
-                <Item item={item} key ={index} onRemove={onRemove}/>
-            ))}
-        </div>
-    )
+function Result({ list, onRemove }) {
+  return (
+    <div>
+      {list.map((item, index) => (
+        <Item item={item} key={index} onRemove={onRemove} />
+      ))}
+    </div>
+  );
 }
-function Item({item,k, onRemove}){
-    return(
-        <div>
-          <label><b>{item}</b><Button id="remove" variant='contained' color="secondary" onClick ={() => onRemove(item)}>x</Button> 
-          </label>
-        </div>
-    )
+function Item({ item, k, onRemove }) {
+  return (
+    <div>
+      <label>
+        <b>{item}</b>
+        <Button
+          id="remove"
+          variant="contained"
+          color="secondary"
+          onClick={() => onRemove(item)}
+        >
+          x
+        </Button>
+      </label>
+    </div>
+  );
 }
-
 
 export default Result;
