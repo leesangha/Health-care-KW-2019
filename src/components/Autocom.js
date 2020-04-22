addc16cf8a5727b057cb41b0b763b0272830bca6
appano1 <thankfather@naver.com>
2020-04-22 18:33:53 +0900

    음식 검색 컴포넌트 생성

--- src/components/Autocom.js
+++ src/components/Autocom.js
@@ -1,77 +1,92 @@
-import React, { useState, useEffect, useCallback } from "react";
-import Autocomplete from "react-autocomplete";
+import React, { useState } from "react";
+import ReactAutocomplete from "react-autocomplete";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./scss/Autocom.scss";
 
-export default function Autocom() {
-  const [source, setSource] = useState({
-    food_list: [],
-  });
+export default function Autocom(props) {
   const [inputs, setInputs] = useState({
     food: "",
   });
 
-  const { food } = inputs;
-  const { food_list } = source;
+  const source = sessionStorage.getItem("foodInfo");
+  let foodList;
 
-  const getFood = () => {
-    fetch("search_ingredient/Allfood", {
-      method: "POST",
-      body: JSON.stringify({}),
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const arr = [];
-        data.forEach((elem) => {
-          arr.push(elem.food_name);
-        });
-        console.log(arr);
-        setSource({
-          ...source,
-          food_list: arr,
-        });
-      });
-  };
-  useEffect(() => {
-    console.log("음식 받아오기");
-    getFood();
-  }, []);
+  if (source !== undefined) {
+    foodList = JSON.parse(source).result.map(({ food_no, food_name }) => ({
+      food_no,
+      food_name,
+    }));
+  } else {
+    props.history.push("/");
+  }
+
+  const { food } = inputs;
 
   const onClick = () => {
     console.log(food);
   };
+
+  const inputProps = {
+    placeholder: "Search...",
+    style: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 10,
+      borderColor: 'black',
+      fontSize: 13,
+      color: '#262626',
+      paddingLeft: 45,
+    },
+  };
+
   return (
-    <div>
-      <Autocomplete
-        style={{ height: 200 }}
-        items={food_list}
-        getItemValue={(item) => item}
-        shouldItemRender={(item, food) =>
-          item.toLowerCase().indexOf(food.toLowerCase()) > -1
+    <div id="search-box">
+      <FontAwesomeIcon icon={faSearch} />
+      <ReactAutocomplete
+        inputProps={inputProps}
+        items={foodList}
+
+        wrapperStyle={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: 50,
+        }}
+
+        menuStyle={{
+          borderRadius: '3px',
+          boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
+          background: "rgba(255, 255, 255, 0.9)",
+          boxSizing: "border-box",
+          padding: "2px 10px",
+          fontSize: "90%",
+          position: "fixed",
+          overflow: "auto",
+          minWidth: '96%',
+          maxHeight: "20%", // TODO: don't cheat, let it flow to the bottom
+        }}
+        shouldItemRender={(item, value) =>
+          item.food_name.toLowerCase().indexOf(value.toLowerCase()) > -1
         }
-        value={food}
-        onChange={(e) =>
-          setInputs({
-            food: e.target.value,
-          })
-        }
-        onSelect={(val) => setInputs({ food: val })}
+        getItemValue={(item) => item.food_name}
         renderItem={(item, highlighted) => (
           <div
-            key={item}
+            key={item.food_no}
             style={{
-              height: 40,
-              backgroundColor: highlighted ? "lightblue" : "transparent",
+              display: 'flex',
+              alignItems: 'center',
+              height: 30,
+              backgroundColor: highlighted ? "#eee" : "transparent",
             }}
           >
-            {item}
+            {item.food_name}
           </div>
         )}
+        value={inputs.food}
+        onChange={(e) => setInputs({ food: e.target.value })}
+        onSelect={(food) => setInputs({ food })}
       />
-      <button onClick={onClick}>검색</button>
     </div>
   );
 }
